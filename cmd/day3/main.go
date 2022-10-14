edec8a6dc8ef0236b13565fdee27ba46427d3e2a
Vishal Kotcherlakota <dishbreak@gmail.com>
2022-10-13 21:04:42 -0700

    finished day 3 part 2
    I used goroutines to handle parsing out the individual triangles. Was it performant? Not really. But it was a nice way to model the problem. A trio of goroutines read values from dedicated channels. They in turn parse the values, and generate triplets which get written to another channel. The parsing channel gets iterated over and writes a final value to a result channel, and the main goroutine waits for and returns the result.

--- cmd/day3/main.go
+++ cmd/day3/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/dishbreak/aoc2020/lib"
 )
@@ -15,6 +16,7 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: %d\n", part2(input))
 }
 
 func part1(input []string) int {
@@ -23,19 +25,93 @@ func part1(input []string) int {
 		if item == "" {
 			continue
 		}
-		if isValid(item) {
+		if validLIne(item) {
 			acc++
 		}
 	}
 	return acc
 }
 
-func isValid(line string) bool {
+func part2(input []string) int {
+	triangleStreams := make([]chan string, 3)
+	for i := range triangleStreams {
+		triangleStreams[i] = make(chan string, len(input))
+	}
+
+	parsed := make(chan [3]int)
+
+	var wg sync.WaitGroup
+	wg.Add(3)
+	for i := range triangleStreams {
+		go func(input chan string) {
+			defer wg.Done()
+
+			count := 0
+			var buf [3]int
+			for {
+				s, ok := <-input
+				if !ok {
+					return
+				}
+				n, _ := strconv.Atoi(s)
+				buf[count] = n
+				count++
+
+				if count == 3 {
+					parsed <- buf
+					buf = [3]int{}
+					count = 0
+				}
+			}
+		}(triangleStreams[i])
+	}
+
+	go func() {
+		wg.Wait()
+		close(parsed)
+	}()
+
+	result := make(chan int)
+	defer close(result)
+
+	go func() {
+		acc := 0
+		for triangle := range parsed {
+			if isValid(triangle) {
+				acc++
+			}
+		}
+		result <- acc
+	}()
+
+	for _, line := range input {
+		if line == "" {
+			continue
+		}
+
+		for i, f := range strings.Fields(line) {
+			triangleStreams[i] <- f
+		}
+	}
+
+	for i := range triangleStreams {
+		close(triangleStreams[i])
+	}
+
+	return <-result
+
+}
+
+func validLIne(line string) bool {
 	var sides [3]int
 	for i, pt := range strings.Fields(line) {
 		sides[i], _ = strconv.Atoi(pt)
 	}
 
+	return isValid(sides)
+}
+
+func isValid(sides [3]int) bool {
 	combos := [][]int{
 		{0, 1, 2},
 		{1, 2, 0},
