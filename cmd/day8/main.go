61c0aabb677662ef3965dc8ad404d9786ba3f4c8
Vishal Kotcherlakota <dishbreak@gmail.com>
2022-10-31 01:04:44 -0700

    day 8 part 2 complete
    this was a pretty light refactor. I changed the code in part 1 to be a helper that returned the TinyScreen, and was then able to take advantage of the String() method I wrote for unit testing. Wazoo!

--- cmd/day8/main.go
+++ cmd/day8/main.go
@@ -13,9 +13,18 @@ func main() {
 	}
 
 	fmt.Printf("Part 1: %d\n", part1(input))
+	fmt.Printf("Part 2: \n%s\n", part2(input))
 }
 
 func part1(input []string) int {
+	return loadScreen(input).PixelsLit()
+}
+
+func part2(input []string) string {
+	return loadScreen(input).String()
+}
+
+func loadScreen(input []string) *TinyScreen {
 	scr := NewTinyScreen()
 
 	for _, line := range input {
@@ -25,5 +34,5 @@ func part1(input []string) int {
 		scr.Inst(line)
 	}
 
-	return scr.PixelsLit()
+	return scr
 }
